a99a3b0e643c53b86bedecc883b5985f5407ae1c
VimukthiKothalawala <VimukthiKothalawala@users.noreply.github.com>
2026-06-13 02:55:52 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict gKvz7ggyKjPsn4x8f7jKCn0mhGqvp4dBZioBdxOLW4u8XskgQxIDbEApZ95QKQC
+-- \restrict WXMjWlebUrC5wQR1ZhXtgZelYfLPuA8BEEwcXxdvrBiPdBDS9Xs0BE9Qn8DMHdC
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -405,6 +405,6 @@ SELECT pg_catalog.setval('"drizzle"."__drizzle_migrations_id_seq"', 1, false);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict gKvz7ggyKjPsn4x8f7jKCn0mhGqvp4dBZioBdxOLW4u8XskgQxIDbEApZ95QKQC
+-- \unrestrict WXMjWlebUrC5wQR1ZhXtgZelYfLPuA8BEEwcXxdvrBiPdBDS9Xs0BE9Qn8DMHdC
 
 RESET ALL;
